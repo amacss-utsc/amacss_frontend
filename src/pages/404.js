3bfaf8b6f04bc125e640c9a4e938638dec1a6bdd
samhaque <sammy.haque@mail.utoronto.ca>
2019-09-16 05:14:35 -0400

    fuck it I'm going to sleep

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,36 @@
-import React from 'react';
-
-import Layout from '@common/Layout';
 import { Container } from '@components/global';
+
+
+import React from 'react';
+import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+import Header from '@sections/Header';
+import About from '@sections/About';
+import Signup from '@sections/Signup';
+import Team from '@sections/Team';
+import Contact from '@sections/Contact';
+import Footer from '@sections/Footer';
+import styled from 'styled-components';
 
 const NotFoundPage = () => (
   <Layout>
-    <Container>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
+    <Navbar/>
+    <Layout>
+      <section>
+        <Container>
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>NOT FOUND</h1>
+            <p>Unfortunately the URL you entered does not exist.</p>
+          </div>
+        </Container>
+      </section>
+    </Layout>
+    <Footer/>
   </Layout>
 );
 
