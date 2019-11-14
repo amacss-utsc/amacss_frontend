8ffd4f53afd247f9ac0fb5d7cd5d794877bc67f8
Keshavaa-S <32556459+Keshavaa-S@users.noreply.github.com>
2019-11-14 00:18:42 -0500

    Started working on events page, not much tho :(

--- src/components/sections/Events.js
+++ src/components/sections/Events.js
@@ -9,7 +9,22 @@ import ExternalLink from '../common/ExternalLink';
 const eventsURL = "https://utsccms.club/";
 
 const Events = () => {
-
+    return (
+        <Section id="events">
+            <Container>
+                <eventContainer>
+                    <h1>hello</h1>
+                </eventContainer>
+            </Container>
+        </Section>
+    )
 }
+
+const eventContainer = styled.div`
+    h1 {
+        font-size: 30px;
+    }
+    background: black;
+`;
 
 export default Events
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Layout from '@common/Layout';
 import Navbar from '@common/Navbar';
 import Header from '@sections/Header';
 import About from '@sections/About';
+
+import Events from '@sections/Events';
+
 import Signup from '@sections/Signup';
 import Contact from '@sections/Contact';
 import Footer from '@sections/Footer';
@@ -13,6 +16,10 @@ const IndexPage = () => (
         <Navbar/>
         <Header/>
         <About/>
+
+        <Events/>
+
+
         <Signup/>
         <Contact/>
         <Footer/>
