c80105967ce962f25efb5f9306971817c2628178
Keshavaa-S <32556459+Keshavaa-S@users.noreply.github.com>
2019-11-13 23:40:05 -0500

    Added Events.js file to sections

---
+++ src/components/sections/Events.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import {graphql, StaticQuery} from 'gatsby';
+import Img from 'gatsby-image';
+
+import {Container, Section} from '@components/global';
+import ExternalLink from '../common/ExternalLink';
+
+const eventsURL = "https://utsccms.club/";
+
+const Events = () => {
+
+}
+
+export default Events
