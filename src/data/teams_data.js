58b348d753bf63ebb2b57397210c5d658058fe8a
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2023-02-07 17:47:35 -0500

    Huge Team Update for 2023!
    - added alumni section
    - organized pictures of current new team!

--- src/data/teams_data.js
+++ src/data/teams_data.js
@@ -8,8 +8,20 @@ const EXECUTIVE_TEAM = [
   {
     name: "Katrina Best",
     image: "Katrina_Best.jpg",
-    role: "VP of Technology/Marketing",
+    role: "Vice President",
     linkedin: "https://www.linkedin.com/in/katrina-best/"
+  },
+  {
+    name: "Aarushi Doshi",
+    image: "Aarushi_Doshi.jpg",
+    role: "VP of Operations",
+    linkedin: "https://www.linkedin.com/in/aarushi-doshi-5901141a7/"
+  },
+  {
+    name: "Danish Siddiqui",
+    image: "Danish_Siddiqui.jpg",
+    role: "VP of External Affairs",
+    linkedin: "https://www.linkedin.com/in/danish-siddiqui-24b3861a3/"
   },
   {
     name: "Vincent Zhang",
@@ -17,14 +29,22 @@ const EXECUTIVE_TEAM = [
     role: "VP of Academics",
     linkedin: "https://www.linkedin.com/in/vincentzhang15/"
   },
-  {
-    name: "Cené Odendaal",
-    image: "ceneodendaal.jpg",
-    role: "Former President",
-    linkedin: "https://www.linkedin.com/in/cen%C3%A9-odendaal-137a18180/"
-  },
 ];
 const ACADEMICS_TEAM = [
+  {
+    name: "Efkan Goktepe",
+    image: "Efkan_Goktepe.jpg",
+    role: "Academic Representative",
+    linkedin: "https://www.linkedin.com/in/serhatgoktepe/",
+    description: ""
+  },
+  {
+    name: "Sung Huang",
+    image: "Sung_Huang.jpg",
+    role: "Academic Representative",
+    linkedin: "https://www.linkedin.com/in/hwangso595/",
+    description: ""
+  },
   {
     name: "Malhar Pandya",
     image: "Malhar_Pandya.jpg",
@@ -40,21 +60,116 @@ const ACADEMICS_TEAM = [
     description: ""
   },
   {
+    name: "Mohannad Shehata",
+    image: "Mohannad_Shehata.png",
+    role: "Academic Representative",
+    linkedin: "https://www.linkedin.com/in/mohannad-shehata/",
+    description: ""
+  },
+  {
     name: "Alex Teeter",
     image: "Alexander_Teeter.png",
     role: "Academic Representative",
     linkedin: "https://www.linkedin.com/in/alexander-teeter-a7b3ba223/",
     description: ""
-  }
+  },
+  {
+    name: "Theresa Yu",
+    image: "Theresa_Yu.jpg",
+    role: "Academic Representative",
+    linkedin: "https://www.linkedin.com/in/theresa-yu-70278119b/",
+    description: ""
+  },
+];
+
+const MARKETING_TEAM = [
+  {
+    name: "Jinie Choi",
+    image: "Jinie_Choi.jpg",
+    role: "Director of Marketing",
+    linkedin: "https://www.linkedin.com/in/emily-choi-a1bba9243/"
+  },
+  {
+    name: "Yihoi Jung",
+    image: "Yihoi_Jung.jpg",
+    role: "Director of Marketing",
+    linkedin: "https://www.linkedin.com/in/yihoi-jung-0b95351b5/"
+  },
 ];
 
 const EVENTS_TEAM = [
   {
+    name: "Ayesha Khan",
+    image: "Ayesha_Khan.jpg",
+    role: "Director of Events",
+    linkedin: "https://www.linkedin.com/in/ayesha-khan-882483215/"
+  },
+];
+
+const FIRSTYEAR_TEAM = [
+  {
+    name: "Alankrit Verma",
+    image: "Alankrit_Verma.png",
+    role: "First Year Representative Coordinator",
+    linkedin: "https://www.linkedin.com/in/alankritverma/"
+  },
+  {
+    name: "Billy Brillian",
+    image: "Billy_Brillian.jpg",
+    role: "First Year Representative",
+    linkedin: "https://www.linkedin.com/in/enrizky-brillian/"
+  },
+  {
+    name: "Sanvi Dubey",
+    image: "Sanvi_Dubey.jpg",
+    role: "First Year Representative",
+    linkedin: "https://www.linkedin.com/in/sanvi-dubey-b77131265/"
+  },
+  {
+    name: "Adib Fallahpour",
+    image: "Adibvafa_Fallahpour.jpg",
+    role: "First Year Representative",
+    linkedin: "https://www.linkedin.com/in/adibvafa-fallahpour/"
+  },
+  {
+    name: "Luna Tran",
+    image: "Luna_Tran.jpg",
+    role: "First Year Representative",
+    linkedin: "https://www.linkedin.com/in/huong-tran-88a989245/"
+  },
+];
+
+const ALUMNI_TEAM = [
+  {
+    name: "Cené Odendaal",
+    image: "ceneodendaal.jpg",
+    role: "2021-2022 President",
+    linkedin: "https://www.linkedin.com/in/cen%C3%A9-odendaal-137a18180/"
+  },
+  {
     name: "Changhao Wang",
     image: "changhaowang.jpg",
     role: "Events Director",
-    linkedin: ""
-  }
+    linkedin: "https://www.linkedin.com/in/changhao-wang-baa8a3211/"
+  },
+  {
+    name: "Clara Chick",
+    image: "clarachick.jpg",
+    role: "VP of Tech",
+    linkedin: "https://www.linkedin.com/in/clarachick/"
+  },
+  {
+    name: "Jason Tian",
+    image: "jasontian.jpg",
+    role: "2020-2021 President",
+    linkedin: "https://www.linkedin.com/in/jtian6/"
+  },
+  {
+    name: "Tabeeb Yeamin",
+    image: "tabeebyeamin.jpg",
+    role: "Academic Rep",
+    linkedin: "https://www.linkedin.com/in/tabeebyeamin/"
+  },
 ];
 
 const SPONSORED_BY = [
@@ -65,4 +180,4 @@ const SPONSORED_BY = [
   }
 ];
 
-export { EXECUTIVE_TEAM, ACADEMICS_TEAM, EVENTS_TEAM, SPONSORED_BY };
+export { EXECUTIVE_TEAM, ACADEMICS_TEAM, MARKETING_TEAM, EVENTS_TEAM, FIRSTYEAR_TEAM, SPONSORED_BY, ALUMNI_TEAM };
