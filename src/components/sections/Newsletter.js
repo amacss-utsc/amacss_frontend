7480906eea814684f041a1b5a9c09556292946a2
murphylee10 <murphylee2004@gmail.com>
2024-11-10 21:02:22 -0500

    fix: attempt to fix weird newsletter glitch

--- src/components/sections/Newsletter.js
+++ src/components/sections/Newsletter.js
@@ -4,15 +4,15 @@ import { Container, Section } from "../../data/global";
 import { Helmet } from "react-helmet";
 
 const Newsletter = () => {
-  useEffect(() => {
-    const formContainers = document.getElementsByClassName("ml-form-container");
-    Array.from(formContainers).forEach((formContainer) => {
-      const embeddedDiv = document.createElement("div");
-      embeddedDiv.className = "ml-embedded";
-      embeddedDiv.setAttribute("data-form", "3M7MKq");
-      formContainer.appendChild(embeddedDiv);
-    });
-  }, []);
+  // useEffect(() => {
+  //   const formContainers = document.getElementsByClassName("ml-form-container");
+  //   Array.from(formContainers).forEach((formContainer) => {
+  //     const embeddedDiv = document.createElement("div");
+  //     embeddedDiv.className = "ml-embedded";
+  //     embeddedDiv.setAttribute("data-form", "3M7MKq");
+  //     formContainer.appendChild(embeddedDiv);
+  //   });
+  // }, []);
 
   return (
     <Section id="newsletter">
@@ -25,18 +25,7 @@ const Newsletter = () => {
             events!
           </StyledParagraph>
           <br />
-          <Helmet>
-            <script>
-              {`
-                (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-                .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-                n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-                (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-                ml('account', '1182403');
-              `}
-            </script>
-          </Helmet>
-          <div className="ml-form-container"></div>
+          <div class="ml-embedded" data-form="3M7MKq"></div>
         </div>
       </Container>
     </Section>
