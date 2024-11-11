0fe2f90cbabb8338fa488c22247f9a67112a597e
murphylee10 <murphylee2004@gmail.com>
2024-11-10 21:51:26 -0500

    put script back

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -20,7 +20,7 @@ const NewsletterPage = () => {
               and events!
             </StyledParagraph>
             <br />
-            <div class="ml-embedded" data-form="3M7MKq"></div>
+            {/* <div class="ml-embedded" data-form="3M7MKq"></div> */}
           </div>
         </Container>
       </Section>
@@ -32,20 +32,20 @@ const StyledParagraph = styled.p`
   margin-bottom: 1.5rem;
 `;
 
-// export function Head() {
-//   return (
-//     <>
-//       <script>
-//         {`
-//           (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-//           .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-//           n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-//           (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-//           ml('account', '1182403');
-//         `}
-//       </script>
-//     </>
-//   );
-// }
+export function Head() {
+  return (
+    <>
+      <script>
+        {`
+          (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+          .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+          n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+          (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+          ml('account', '1182403');
+        `}
+      </script>
+    </>
+  );
+}
 
 export default NewsletterPage;
